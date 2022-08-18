2eae464a94d9298b95046ceb9a6ea556dac18c80
MerveKarakurt94 <merve.karakurt@live.de>
2022-08-18 14:10:54 +0200

    arrange data by variables country (cntry) and income (hinctnta) and count income deciles

--- household_income.py
+++ household_income.py
@@ -1,31 +1,8 @@
-import csv
-import collections
-from countries import country_column
+import pandas as pd
 
-BG = collections.Counter()
-income_BG = []
-count_BG = []
+ess10_df = pd.read_csv("ESS10.csv")
+country_income_df = ess10_df[["cntry", "hinctnta"]]
+filtered_income_df = country_income_df[country_income_df["hinctnta"] <= 10.0]
+counted_income_df = filtered_income_df.groupby(["cntry","hinctnta"])["hinctnta"].count().rename("count").reset_index()
 
-
-with open("ESS10.csv", newline="") as csvfile:
-    reader = csv.reader(csvfile, delimiter=",")
-    header = next(reader)
-    income_column = header.index("hinctnta")
-    print(income_column)
-
-    # The following values do not represent any interesting answer by a respondent
-    no_answer = ["77.000000", "88.000000", "99.000000"]
-
-    for line in reader:
-        if line[income_column] in no_answer:
-            continue
-        if line[country_column] == "BG":
-            BG[float(line[income_column])] += 1
-
-    for key in BG:
-        value = BG[key]
-        income_BG.append(key)
-        count_BG.append(value)
-
-print(income_BG)
-print(count_BG)
+print(counted_income_df)
