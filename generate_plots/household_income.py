d240d9449a100ecf747f50ea33774eb5072d5124
MerveKarakurt94 <merve.karakurt@live.de>
2022-08-25 16:21:11 +0200

    work on website (html, css, descriptions)

--- generate_plots/household_income.py
+++ generate_plots/household_income.py
@@ -5,7 +5,6 @@ ess10_df = pd.read_csv("../ESS10.csv")
 hh_income = ess10_df[["cntry", "hinctnta"]]
 filtered_hh_income = hh_income[hh_income["hinctnta"] <= 10.0]
 counted_hh_income = filtered_hh_income.groupby(["cntry","hinctnta"])["hinctnta"].count().rename("Count").reset_index()
-print(counted_hh_income)
 
 fig = px.bar(counted_hh_income, x="cntry", y="Count", color="hinctnta", title="Figure 2: Household Income Deciles",
              labels={"cntry": "Country", "hinctnta": "Decile"})
